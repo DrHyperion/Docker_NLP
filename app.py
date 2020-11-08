63abb278591bdf1dfa98702d4bdf86762e6a4067
DrHyperion <david.passdel@laposte.net>
2020-11-08 16:43:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from nltk.stem.porter import PorterStemmer
 
-
 import pandas as pd 
 import numpy as np
 import re
